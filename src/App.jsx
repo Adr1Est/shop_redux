1304ea8162cfaea6a1a601a35051aff8bcaf9ae8
Adrián Estévez Salamanca <adrianestevezsalamanca@gmail.com>
2025-08-07 11:53:55 +0200

    allProductsSlice working

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ function App() {
 
   return (
     <>
-      <MostrarLista/>
+      <div className='bg-gray-800 min-h-dvh flex justify-center items-center'>
+        <MostrarLista/>
+      </div>
     </>
   )
 }
--- src/components/MostrarLista.jsx
+++ src/components/MostrarLista.jsx
@@ -1,10 +1,11 @@
 import { useDispatch, useSelector } from "react-redux"
 import { fetchMockData } from "../features/allProducts/allProductsSlice"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 function MostrarLista(){
+  const [containerClass, setContainerClass] = useState("h-150 w-full md:w-1/2 lg:w-1/4 bg-purple-900 text-gray-50 border-2 border-purple-400 rounded-2xl p-3 overflow-y-auto")
   const dispatch = useDispatch()
-  const status = useSelector(state => state.allProducts.state)
+  const status = useSelector(state => state.allProducts.status)
   const mockData = useSelector(state => state.allProducts.allProducts)
 
   useEffect(() => {
@@ -12,17 +13,26 @@ function MostrarLista(){
       dispatch(fetchMockData())
       console.log('Datos solicitados')
     }
+    console.log(status)
   }, [status, dispatch])
 
   useEffect(() => {
     console.log(mockData)
   }, [mockData])
+
+  if(mockData.length === 0){
+    return (
+      <div className={containerClass}>
+        <li>Sin datos</li>
+      </div>
+    )
+  }
+
   return(
-    <div>
-      <ul>
-        {mockData.length > 0
-          ? mockData.map(product => <li>{product.title}</li>)
-          : <li>Sin datos</li>
+    <div className={containerClass}>
+      <ul className="list-disc">
+        {
+          mockData.map((product, index) => <li className="mb-2 bg-purple-950 rounded-2xl p-2" key={index}>{product.title}</li>)
         }
       </ul>
     </div>
